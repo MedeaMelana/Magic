1b2d9efaa8d9d1a63bacfe6544728b5f0a22f1ec
Martijn van Steenbergen <martijn@van.steenbergen.nl>
2012-11-04 16:41:24 +0100

    Initial docs for module IdList

--- IdList.hs
+++ IdList.hs
@@ -1,9 +1,20 @@
 {-# LANGUAGE TypeOperators #-}
 
-module IdList
-  ( Id, IdList
-  , empty, head, get, set, remove, cons, fromList, toList, ids, filter, shuffle
-  , consM, removeM, shuffleM
+module IdList (
+
+    -- * Types
+    Id, IdList,
+
+    -- * Construction
+    empty, fromList,
+
+    -- * Querying
+    head, get, toList, ids,
+
+    -- * Modifying
+    set, remove, cons, filter, shuffle,
+    consM, removeM, shuffleM
+
   ) where
 
 import Prelude hiding (filter, head)
@@ -16,6 +27,11 @@ import Data.Label.Pure ((:->))
 import Data.Label.PureM (gets, puts)
 import System.Random.Shuffle (shuffleM)
 
+
+
+-- TYPES
+
+
 type Id = Int
 
 data IdList a = IdList [(Id, a)] Id
@@ -23,8 +39,21 @@ data IdList a = IdList [(Id, a)] Id
 instance Functor IdList where
   fmap = contents . fmap . second
 
+
+
+-- CONSTRUCTION
+
+
 empty :: IdList a
 empty = IdList [] 0
+
+fromList :: [a] -> IdList a
+fromList = foldr (\x xs -> snd (cons x xs)) empty
+
+
+
+-- QUERYING
+
 
 head :: IdList a -> Maybe (Id, a)
 head (IdList (ix : _) _) = Just ix
@@ -32,6 +61,17 @@ head _ = undefined
 
 get :: Id -> IdList a -> Maybe a
 get i (IdList ixs _) = lookup i ixs
+
+toList :: IdList a -> [(Id, a)]
+toList (IdList ixs _) = ixs
+
+ids :: IdList a -> [Id]
+ids = map fst . toList
+
+
+
+-- MODIFYING
+
 
 set :: Id -> a -> IdList a -> IdList a
 set i x (IdList ixs ni) = IdList (map f ixs) ni
@@ -46,24 +86,15 @@ remove i l =
     Just x  -> Just (x, contents (Prelude.filter (\(i', _) -> i /= i')) l)
     Nothing -> Nothing
 
-pop :: IdList a -> Maybe (a, IdList a)
-pop (IdList ((_, x) : ixs) i) = Just (x, IdList ixs i)
-pop _ = Nothing
+--pop :: IdList a -> Maybe (a, IdList a)
+--pop (IdList ((_, x) : ixs) i) = Just (x, IdList ixs i)
+--pop _ = Nothing
 
 cons :: a -> IdList a -> (Id, IdList a)
 cons x (IdList ixs i) = (i, IdList ((i, x) : ixs) (succ i))
 
 contents :: ([(Id, a)] -> [(Id, b)]) -> IdList a -> IdList b
 contents f (IdList ixs i) = IdList (f ixs) i
-
-fromList :: [a] -> IdList a
-fromList = foldr (\x xs -> snd (cons x xs)) empty
-
-toList :: IdList a -> [(Id, a)]
-toList (IdList ixs _) = ixs
-
-ids :: IdList a -> [Id]
-ids = map fst . toList
 
 filter :: (a -> Bool) -> IdList a -> [(Id, a)]
 filter f = Prelude.filter (f . snd) . toList
