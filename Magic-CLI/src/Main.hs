19ea3811b32914bd22adef0ac2613b75ec3e3a9a
Eckhard Maass <eckhard.s.maass@googlemail.com>
2016-09-04 13:35:12 +0200

    Add different decks to cli.

--- Magic-CLI/src/Main.hs
+++ Magic-CLI/src/Main.hs
@@ -7,8 +7,10 @@ import Magic.M13
 import Magic.AvacynRestored
 
 import Data.Monoid ((<>))
+import Data.Maybe (fromMaybe, listToMaybe)
 
 import System.IO
+import System.Environment
 
 redDeck :: Deck
 redDeck = replicate 18 mountain <> replicate 21 bloodArtist <> replicate 21 searingSpear
@@ -24,7 +26,14 @@ whiteDeck = replicate 24 plains
          -- <> replicate 4 captain'sCall
          -- <> replicate 4 divineFavor
 
+planarCleaninsingDeck :: Deck
+planarCleaninsingDeck = replicate 15 planarCleansing <> replicate 15 pillarfieldOx <> replicate 15 tormod'sCrypt
+
+decks = [("planarCleaninsingDeck", planarCleaninsingDeck), ("whiteDeck", whiteDeck)]
+
 main :: IO ()
 main = do
+  args <- getArgs
+  let secondDeck = fromMaybe whiteDeck $ listToMaybe args >>= (flip lookup) decks
   hSetBuffering stdout NoBuffering
-  runGame [redDeck, whiteDeck]
+  runGame [redDeck, secondDeck]
